adc3fb5dd10936cdb3daddc05311be351dd42ef8
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-11 13:11:24 +0530

    cc

---
+++ Team.java
@@ -0,0 +1,8 @@
+public class Team
+{
+    public static void main(String args[] )
+    {
+        names t1= new names();
+        t1.name();
+    }
+}
---
+++ names.java
@@ -0,0 +1,10 @@
+public class names {
+    public void name()
+    {
+        System.out.println("Mani");
+        System.out.println("Karthik");
+        System.out.println("Fayaz");
+        System.out.println("Mahesh");
+    }
+    
+}
